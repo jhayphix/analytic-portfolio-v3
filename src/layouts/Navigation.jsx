dbb3e6697f9bbff54cb08ba48dfc5fe495b85d54
Lucid Macbook <Lucid@Lucids-MacBook-Pro.local>
2024-10-04 18:01:23 +0000

    Add : WebDev portfolio url to top navigation

--- src/layouts/Navigation.jsx
+++ src/layouts/Navigation.jsx
@@ -27,7 +27,7 @@ const Navigation = () => {
   |----------------------------------------
   */
 
-  const { nav_links, home } = useContext(NavigationContext);
+  const { nav_links, home, web_dev_url } = useContext(NavigationContext);
   const { switchTheme, theme } = useContext(ThemeContext);
   const [nav_scrolled, setNavScrolled] = useState(false);
   const [link_clicked, setLinkClicked] = useState(false);
@@ -150,6 +150,17 @@ const Navigation = () => {
                   </li>
                 );
               })}
+
+              <li className="nav-item side_nav_item mt-4">
+                <a
+                  className="nav-link side_nav_link py-1 ps-1 text-center text-decoration-underline"
+                  href={web_dev_url}
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  View WebDev Portfolio
+                </a>
+              </li>
             </ul>
           </div>
         </div>
